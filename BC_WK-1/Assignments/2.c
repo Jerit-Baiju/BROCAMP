a63013a0973e37f035f0ab5df9bf64f7598ceb44
Jerit Baiju <jeritalumkal@gmail.com>
2022-06-21 14:20:23 +0530

    solved Q-2

--- BC_WK-1/Assignments/2.c
+++ BC_WK-1/Assignments/2.c
@@ -5,7 +5,7 @@ int main()
     int num_1;
     float num_2, sum;
     printf("Enter two numbers ");
-    scanf("%d%f", &num_1,&num_2);
+    scanf("%d%f", &num_1, &num_2);
     sum = num_1 + num_2;
     printf("the sum is %f", sum);
     return 0;
